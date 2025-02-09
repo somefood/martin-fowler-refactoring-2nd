dc5fba3a9b5ab8968bb49a4e39346c0682d18674
somefood <iamseokjuhong@gmail.com>
2025-02-09 13:40:16 +0900

    1.1 코드 작성

---
+++ chp01/main.js
@@ -0,0 +1,6 @@
+const invoices = require('./invoices.json');
+const plays = require('./plays.json');
+const statement = require("./statemet");
+
+const result = statement(invoices[0], plays);
+console.log(result);
