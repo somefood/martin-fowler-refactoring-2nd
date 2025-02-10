bd6f4c628884d988b9daa8a10223fc7b117f1dcd
somefood <iamseokjuhong@gmail.com>
2025-02-10 23:15:33 +0900

    1.4 totalVolumeCredits 함수 추출

--- chp01/statemet.js
+++ chp01/statemet.js
@@ -1,14 +1,16 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
+
+    let volumeCredits = totalVolumeCredits();
+
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
@@ -55,6 +57,14 @@ function statement(invoice, plays) {
                 minimumFractionDigits: 2
             }
         ).format(aNumber / 100);
+    }
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+        return volumeCredits;
     }
 }
 
