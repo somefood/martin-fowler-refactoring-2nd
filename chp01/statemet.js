650a683f2b113010740254c304703f1bc1218cf0
somefood <iamseokjuhong@gmail.com>
2025-02-10 22:49:29 +0900

    1.4 statemet 함수 쪼개기

--- chp01/statemet.js
+++ chp01/statemet.js
@@ -13,23 +13,7 @@ function statement(invoice, plays) {
         const play = plays[perf.playID];
         let thisAmount = 0;
 
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`);
-        }
+        thisAmount = amountFor(perf, play);
         // 포인트를 적립한다
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 희극 관객 5명마다 추가 포인트를 제공한다.
@@ -42,6 +26,28 @@ function statement(invoice, plays) {
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+}
+
+function amountFor(perf, play) {
+    let thisAmount = 0;
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+    return thisAmount;
 }
 
 module.exports = statement;
