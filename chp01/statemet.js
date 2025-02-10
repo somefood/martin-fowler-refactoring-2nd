a345541dfb96629fa90ddadb754bed76826a3d96
somefood <iamseokjuhong@gmail.com>
2025-02-10 22:56:03 +0900

    1.4 playFor 함수 구현

--- chp01/statemet.js
+++ chp01/statemet.js
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = 0;
 
         thisAmount = amountFor(perf, play);
@@ -26,28 +26,32 @@ function statement(invoice, plays) {
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
-}
 
-function amountFor(aPerformance, play) {
-    let result = 0;
-    switch (play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
+    function amountFor(aPerformance, play) {
+        let result = 0;
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`);
+        }
+        return result;
     }
-    return result;
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
 }
 
 module.exports = statement;
