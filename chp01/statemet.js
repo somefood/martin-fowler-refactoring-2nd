ec95397f2dd5456de73c6c3cd76f00af07d6832d
somefood <iamseokjuhong@gmail.com>
2025-02-10 23:21:08 +0900

    1.4 totalAmount 함수 추출

--- chp01/statemet.js
+++ chp01/statemet.js
@@ -1,15 +1,13 @@
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
 
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 
@@ -63,6 +61,14 @@ function statement(invoice, plays) {
             volumeCredits += volumeCreditsFor(perf);
         }
         return volumeCredits;
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
     }
 }
 
