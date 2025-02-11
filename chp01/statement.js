51e825792df7a91bd0c868447aa44f231dcfb504
somefood <iamseokjuhong@gmail.com>
2025-02-11 22:28:19 +0900

    1.5 totalAmount, totalVolumeCredits 함수 이동

--- chp01/statement.js
+++ chp01/statement.js
@@ -2,6 +2,8 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance) {
@@ -46,9 +48,25 @@ function statement(invoice, plays) {
         }
         return result;
     }
+
+    function totalAmount(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.amount;
+        }
+        return result;
+    }
+
+    function totalVolumeCredits(data) {
+        let volumeCredits = 0;
+        for (let perf of data.performances) {
+            volumeCredits += perf.volumeCredits;
+        }
+        return volumeCredits;
+    }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
 
     for (let perf of data.performances) {
@@ -57,8 +75,8 @@ function renderPlainText(data, plays) {
         result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
     }
 
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
 
     function usd(aNumber) {
@@ -68,22 +86,6 @@ function renderPlainText(data, plays) {
                 minimumFractionDigits: 2
             }
         ).format(aNumber / 100);
-    }
-
-    function totalVolumeCredits() {
-        let volumeCredits = 0;
-        for (let perf of data.performances) {
-            volumeCredits += perf.volumeCredits;
-        }
-        return volumeCredits;
-    }
-
-    function totalAmount() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
     }
 }
 
