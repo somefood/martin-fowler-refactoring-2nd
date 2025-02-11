635de30fe503e92cac3684669c4a9b5b419c1bb2
somefood <iamseokjuhong@gmail.com>
2025-02-11 22:50:26 +0900

    1.5 htmlStatement 작성

--- chp01/main.js
+++ chp01/main.js
@@ -1,6 +1,8 @@
 import invoices from './invoices.json' with {type: "json"};
 import plays from './plays.json' with {type: "json"};
-import statement from "./statement.js";
+import {statement, htmlStatement} from "./statement.js";
 
 const result = statement(invoices[0], plays);
 console.log(result);
+console.log("================");
+console.log(htmlStatement(invoices[0], plays));
